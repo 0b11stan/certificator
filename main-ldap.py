c9ec899df8e4536f0362bdd8fae99edbc02fc0ab
TristanPinaudeau <tristan@flexfactory.fr>
2019-05-05 11:34:55 +0200

    feat: implement jwt authenticaiton

---
+++ main-ldap.py
@@ -0,0 +1,15 @@
+#from flask_simpleldap import LDAP
+
+#app = Flask(__name__)
+#
+#app.debug = True
+#app.secret_key = "S3CR3T"
+#app.config['LDAP_HOST'] = 'ad-crypto.epsi.intra'
+#app.config['LDAP_BASE_DN'] = 'cn=Users,dc=epsi,dc=intra'
+#app.config['LDAP_USERNAME'] = 'cn=Administrator,cn=Users,dc=epsi,dc=intra'
+#app.config['LDAP_PASSWORD'] = 'P@ssw0rd'
+#app.config['LDAP_USER_OBJECT_FILTER'] = '(sAMAccountName=%s)'
+#
+#ldap = LDAP(app)
+
+
